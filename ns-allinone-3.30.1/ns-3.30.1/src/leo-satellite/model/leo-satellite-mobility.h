4b79b3aa0a2e1bf238079404ac33eeefb3198d6a
anysam <anysamanhas@gmail.com>
2020-03-25 18:00:05 -0700

    updated header for leo-satellite-mobility-model

--- ns-allinone-3.30.1/ns-3.30.1/src/leo-satellite/model/leo-satellite-mobility.h
+++ ns-allinone-3.30.1/ns-3.30.1/src/leo-satellite/model/leo-satellite-mobility.h
@@ -2,9 +2,25 @@
 #ifndef LEO_SATELLITE_MOBILITY_H
 #define LEO_SATELLITE_MOBILITY_H
 
+#include "ns3/object.h"
+#include "ns3/ptr.h"
+#include "ns3/nstime.h"
+#include "ns3/mobility-model.h"
+
 namespace ns3 {
 
-/* ... */
+class LeoSatelliteMobilityModel : public MobilityModel
+{
+public:
+
+private:
+  double m_latitude;
+  double m_longitude;
+  double m_altitude; //kilometers
+  bool m_direction; //each plane will have opposited directions
+  
+  
+}
 
 }
 
